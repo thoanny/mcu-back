3b97a9b89d0a4cb2f73d07d2188150578cb3a9bc
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-10 12:29:55 +0200

    Added Platform add/edit/delete in admin

---
+++ src/Controller/Admin/PlatformController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Platform;
+use App\Form\Admin\PlatformType;
+use App\Repository\PlatformRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlatformController extends AbstractController
+{
+    private PlatformRepository $platformRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(PlatformRepository $platformRepository, EntityManagerInterface $entityManager)
+    {
+        $this->platformRepository = $platformRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/admin/platforms', name: 'app_admin_platforms')]
+    public function index(): Response
+    {
+        $platforms = $this->platformRepository->findAll();
+        return $this->render('admin/platform/index.html.twig', [
+            'platforms' => $platforms,
+        ]);
+    }
+
+    #[Route('/admin/platforms/edit/{id}', name: 'app_admin_platform_edit', defaults: ['id' => null])]
+    public function edit(Request $request, $id): Response
+    {
+        if($id) {
+            $platform = $this->platformRepository->findOneBy(['id' => $id]);
+            if(!$platform) {
+                $this->addFlash('error', 'Platform not found');
+                return $this->redirectToRoute('app_admin_platforms');
+            }
+        } else {
+            $platform = new Platform();
+        }
+
+        $form = $this->createForm(PlatformType::class, $platform);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $platform = $form->getData();
+            $this->entityManager->persist($platform);
+            $this->entityManager->flush();
+            $this->addFlash('success', 'Platform saved');
+            return $this->redirectToRoute('app_admin_platforms');
+        }
+
+        return $this->render('admin/platform/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/admin/platforms/delete/{id}', name: 'app_admin_platform_delete')]
+    public function delete($id): Response
+    {
+        $platform = $this->platformRepository->findOneBy(['id' => $id]);
+        if(!$platform) {
+            $this->addFlash('error', 'Platform not found');
+            return $this->redirectToRoute('app_admin_platforms');
+        }
+
+        $this->entityManager->remove($platform);
+        $this->entityManager->flush();
+        $this->addFlash('success', 'Platform deleted');
+        return $this->redirectToRoute('app_admin_platforms');
+    }
+}
