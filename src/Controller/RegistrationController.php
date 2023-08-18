5a57c4c731520fc3682fd3ae0f6e5f7f6b21815f
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-18 19:29:17 +0200

    Added Mailjet for Mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,12 +49,11 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('hello@anthony-destenay.fr', 'Thoanny'))
+                    ->from(new Address('hello@thoanny.fr', 'Thoanny'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_home');
         }
