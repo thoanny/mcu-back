cd5c30e819864efee304e32e1af16ac6d5680483
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-21 10:28:56 +0200

    Added Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,8 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
